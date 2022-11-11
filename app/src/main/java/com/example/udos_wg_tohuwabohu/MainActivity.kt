4a863f07c49dfdc1536dd5ecbba2b775145ebc42
MatthiasHeilmann <MatthiasHeilmann@hotmail.com>
2022-11-11 12:14:19 +0100

    restart

--- app/src/main/java/com/example/udos_wg_tohuwabohu/MainActivity.kt
+++ app/src/main/java/com/example/udos_wg_tohuwabohu/MainActivity.kt
@@ -22,28 +22,17 @@ import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-    // Firebase
-    private lateinit var oneTapClient: SignInClient
-    private lateinit var signInRequest: BeginSignInRequest
 
-    private val REQ_ONE_TAP = 2  // Can be any integer unique to the Activity
-    private var showOneTapUI = true
 
-//    private val RC_SIGN_IN = 9001
-//
-//    var gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//        .requestEmail()
-//        .build();
-//    var mGoogleApiClient = GoogleApiClient.Builder(this)
-//        .enableAutoManage(this,null)
-//        .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
-//        .build();
-// help pls
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -60,89 +49,7 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
-        val button_login = findViewById(R.id.button_login) as Button
-        button_login.setOnClickListener{
-            when{
-
-            }
-        }
-
-
-        //Firebase
-//        oneTapClient = Identity.getSignInClient(this)
-//        signInRequest = BeginSignInRequest.builder()
-//            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
-//                .setSupported(true)
-//                .build())
-//            .setGoogleIdTokenRequestOptions(
-//                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
-//                    .setSupported(true)
-//                    // Your server's client ID, not your Android client ID.
-//                    .setServerClientId("AIzaSyCHXOBy_UQW99Occ14ZrptcIv5kK9iwW7U")
-//                    // Only show accounts previously used to sign in.
-//                    .setFilterByAuthorizedAccounts(true)
-//                    .build())
-//            // Automatically sign in when exactly one credential is retrieved.
-//            .setAutoSelectEnabled(true)
-//            .build()
-
-//        oneTapClient.beginSignIn(signInRequest)
-//            .addOnSuccessListener(this) { result ->
-//                try {
-//                    startIntentSenderForResult(
-//                        result.pendingIntent.intentSender, REQ_ONE_TAP,
-//                        null, 0, 0, 0, null)
-//                } catch (e: IntentSender.SendIntentException) {
-//                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
-//                }
-//            }
-//            .addOnFailureListener(this) { e ->
-//                // No saved credentials found. Launch the One Tap sign-up flow, or
-//                // do nothing and continue presenting the signed-out UI.
-//                Log.d(TAG, e.localizedMessage)
-//            }
-//            var result22 = onActivityResult(REQ_ONE_TAP,0,null)
-//        print(result22)
     }
-
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//        super.onActivityResult(requestCode, resultCode, data)
-//
-//        when (requestCode) {
-//            REQ_ONE_TAP -> {
-//                try {
-//                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
-//                    val idToken = credential.googleIdToken
-//                    val username = credential.id
-//                    val password = credential.password
-//                    when {
-//                        idToken != null -> {
-//                            // Got an ID token from Google. Use it to authenticate
-//                            // with your backend.
-//                            Log.d(TAG, "Got ID token.")
-//                        }
-//                        password != null -> {
-//                            // Got a saved username and password. Use them to authenticate
-//                            // with your backend.
-//                            Log.d(TAG, "Got password.")
-//                        }
-//                        else -> {
-//                            // Shouldn't happen.
-//                            Log.d(TAG, "No ID token or password!")
-//                        }
-//                    }
-//                } catch (e: ApiException) {
-//                    // ...
-//                }
-//            }
-//        }
-//    }
-
-
-//    fun signIn(){
-//        var signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient)
-//    startActivityForResult(signInIntent,RC_SIGN_IN)
-//    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
