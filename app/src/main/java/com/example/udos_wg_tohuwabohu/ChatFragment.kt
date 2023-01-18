d3fbf44b7f7b33456989128e028e20a9259a5e8f
Ynfiniti <sean_schwarz@gmx.de>
2023-01-18 13:58:13 +0100

    Chat Layout

--- app/src/main/java/com/example/udos_wg_tohuwabohu/ChatFragment.kt
+++ app/src/main/java/com/example/udos_wg_tohuwabohu/ChatFragment.kt
@@ -1,29 +1,35 @@
 package com.example.udos_wg_tohuwabohu
 
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import com.example.udos_wg_tohuwabohu.dataclasses.ChatMessage
 import com.example.udos_wg_tohuwabohu.dataclasses.DataHandler
+import java.util.*
 
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_CHAT_LIST = "chatList"
@@ -34,8 +40,7 @@ private const val ARG_CHAT_LIST = "chatList"
  * create an instance of this fragment.
  */
 class ChatFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+
     private val dataHandler = DataHandler.getInstance()
     private var chatList: Array<ChatMessage>? = null
 
@@ -51,10 +56,12 @@ class ChatFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Inflate the layout for this fragment
         return ComposeView(requireContext()).apply {
             setContent {
-                Text(text = "Hello world.")
+                println("Got Chat list")
+                println(chatList)
+                chatList?.forEach { msg -> println(msg.message) }
+                chatList?.let { ChatBox(messages = it) }
             }
         }
     }
@@ -68,24 +75,82 @@ class ChatFragment : Fragment() {
          * @return A new instance of fragment ChatFragment.
          */
         @JvmStatic
-        fun newInstance(vararg chatMessages: ChatMessage) =
-            ChatFragment().apply {
+        fun newInstance(vararg chatMessages: ChatMessage): Fragment {
+            return ChatFragment().apply {
                 arguments = Bundle().apply {
                     putSerializable(ARG_CHAT_LIST, chatMessages)
                 }
             }
+        }
+    }
+
+    fun formatDate(date: Date): String {
+        return "" + formatNumber(date.hours) + ":" + formatNumber(date.minutes)
+    }
+
+    fun formatNumber(n: Int): String {
+        return if (n > 9) "" + n else "0" + n
     }
 
     @Composable
-    fun chatFragment(messages: Array<ChatMessage>){
-        messages.forEach { msg ->
-            MessageCard(msg)
+    fun ChatBox(vararg messages: ChatMessage) {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .verticalScroll(
+                    enabled = true,
+                    state = ScrollState(0),
+                    reverseScrolling = true
+                )
+                .weight(10f, false)
+            ) {
+                messages.forEach { msg ->
+                    MessageCard(msg)
+                }
+            }
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f, false),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                MessageInput()
+            }
         }
     }
 
     @Composable
+    fun MessageInput() {
+        // TODO get input event and upload to database
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+
+        TextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = { Text(text = "Message") },
+            placeholder = { Text(text = "Type a message") },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Send,
+                    contentDescription = "sendIcon"
+                )
+            }
+        )
+    }
+
+    @Composable
     fun MessageCard(msg: ChatMessage) {
-        Row(modifier = Modifier.padding(all = 8.dp)) {
+        Row(
+            modifier = Modifier.padding(all = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Image(
                 painter = painterResource(R.drawable.ic_launcher_foreground),
                 contentDescription = null,
@@ -94,60 +159,36 @@ class ChatFragment : Fragment() {
                     .clip(CircleShape)
                     .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
             )
+
             Spacer(modifier = Modifier.width(8.dp))
 
-            Column {
+            Column(modifier = Modifier.width(IntrinsicSize.Max)) {
                 Text(
-                    text = dataHandler.getRoommate(msg.user)?.username?: "",
+                    text = dataHandler.getRoommate(msg.user.toString())?.username ?: "unknown",
                     color = MaterialTheme.colors.secondaryVariant,
                     style = MaterialTheme.typography.subtitle2
                 )
 
                 Spacer(modifier = Modifier.height(4.dp))
 
-                Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Surface(shape = MaterialTheme.shapes.medium, elevation = 5.dp) {
                     Text(
-                        text = msg.message?: "",
+                        text = msg.message ?: "",
                         modifier = Modifier.padding(all = 4.dp),
                         style = MaterialTheme.typography.body2
                     )
                 }
-            }
-        }
-    }
 
-    @Composable
-    fun GreetingBox(msg: ChatMessage){
-        Box(modifier = Modifier.wrapContentSize()){
-            Column(modifier = Modifier.wrapContentSize()){
+                Spacer(modifier = Modifier.height(1.dp))
 
                 Text(
-                    text = dataHandler.getRoommate(msg.user)?.username?: "",
-                    color = MaterialTheme.colors.secondaryVariant,
-                    style = MaterialTheme.typography.subtitle2
+                    text = formatDate(msg.timestamp!!),
+                    modifier = Modifier.fillMaxWidth(),
+                    style = TextStyle(
+                        textAlign = TextAlign.Right,
+                        fontSize = 12.sp
+                    )
                 )
-
-                Spacer(modifier = Modifier.height(4.dp))
-
-                Surface(elevation = 10.dp) {
-                    Text(
-                        text = msg.message?: "",
-                        modifier = Modifier.padding(all = 4.dp),
-                        style = MaterialTheme.typography.body2
-                    )
-                }
-                Spacer(modifier = Modifier.height(5.dp))
-                Box(){
-                    Text(
-                        text= msg.timestamp.toString(),
-                        modifier = Modifier.align(Alignment.TopEnd)
-                    )
-                    Text(
-                        "Leo",
-                        modifier = Modifier
-                            .padding(end = 150.dp)
-                    )
-                }
             }
         }
     }
@@ -155,7 +196,29 @@ class ChatFragment : Fragment() {
     @Preview(showBackground = true)
     @Composable
     fun GreetingBoxPreview() {
+        val msgArr = arrayOf(
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Jo was geht", Date(), null),
+            ChatMessage("", "Jo was geht", Date(), null),
+            ChatMessage("", "Jo was geht", Date(), null),
+            ChatMessage("", "Jo was geht", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null),
+            ChatMessage("", "Hello there", Date(), null)
+        )
 
+        ChatBox(*msgArr)
     }
 
 }
