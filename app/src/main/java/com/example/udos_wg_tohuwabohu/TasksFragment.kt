b07fb97672162591a3ea399d42285e86bdcc12ce
MatthiasHeilmann <MatthiasHeilmann@hotmail.com>
2023-01-21 16:48:01 +0100

    delete crap

--- app/src/main/java/com/example/udos_wg_tohuwabohu/TasksFragment.kt
+++ app/src/main/java/com/example/udos_wg_tohuwabohu/TasksFragment.kt
@@ -23,28 +23,18 @@ import com.example.udos_wg_tohuwabohu.dataclasses.Task
 import com.google.android.material.tabs.TabLayout.TabGravity
 import java.util.*
 
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [TasksFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class TasksFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
     lateinit var composeView: ComposeView
     val TAG: String = "[TASKS FRAGMENT]"
     var tasksData = DataHandler.getInstance().getTasks()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -69,16 +59,6 @@ class TasksFragment : Fragment() {
 
     @Composable
     fun TaskCard(taskTitle:String, dueDate: String, frequency:String, roommate: Roommate?){
-//        UdosTheme {
-//            Card(colors= UdoCardTheme(),modifier = Modifier.requiredHeight(height = 80.dp)) {
-//                Row {
-//                    Card(colors = UdoDateCardTheme()) {
-//                        Text(text = i,style = MaterialTheme.typography.displayMedium)
-//                    }
-//                    Text(text = cardText, style = MaterialTheme.typography.displayMedium) }
-//            }
-//        }
-//        UdosTheme {
             Card(colors = UdoCardTheme(), modifier = Modifier
 //                .requiredHeight(height = 100.dp)
 //                .requiredWidthIn(300.dp, 300.dp)
@@ -132,24 +112,6 @@ class TasksFragment : Fragment() {
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TasksFragment.
-         */
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TasksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
     fun getErlediger(){
         //TODO Wer muss die nächste Aufgabe erledigen? brauchen hierfür eine Regel
         //evtl. der, der am wenigesten guteNudel punkte hat oder gerade am wenigsten aufgaben hat?
