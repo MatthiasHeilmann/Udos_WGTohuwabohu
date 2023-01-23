7763ad9ce75d0e89c9d2c7b475dbc283587c3796
Ynfiniti <sean_schwarz@gmx.de>
2023-01-23 11:01:48 +0100

    Fix Comparison with equals function

--- app/src/main/java/com/example/udos_wg_tohuwabohu/dataclasses/ChatMessage.kt
+++ app/src/main/java/com/example/udos_wg_tohuwabohu/dataclasses/ChatMessage.kt
@@ -4,7 +4,7 @@ import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
 import java.util.Date
 
-class ChatMessage(val docId: String, var message: String?, var timestamp: Date?, var user: DocumentReference?) {
+class ChatMessage(val docID: String, var message: String?, var timestamp: Date?, var user: DocumentReference?) {
     constructor(vals: DocumentSnapshot): this(
         vals.id, null, null, null
     )
@@ -15,5 +15,12 @@ class ChatMessage(val docId: String, var message: String?, var timestamp: Date?,
         this.message = vals.getString("message")
         this.timestamp = vals.getTimestamp("timestamp")?.toDate()
         this.user = vals.getDocumentReference("user")
+    }
+
+    fun equals(other: ChatMessage?): Boolean {
+        if (other != null) {
+            return this.docID == other.docID
+        }
+        return false
     }
 }
