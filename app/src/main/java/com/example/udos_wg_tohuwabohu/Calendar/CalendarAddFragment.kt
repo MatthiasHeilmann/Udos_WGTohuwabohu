d7a1a7eb63dc4291ba1942402a5489f72803e850
Ynfiniti <sean_schwarz@gmx.de>
2023-01-31 16:33:06 +0100

    Date and Time picker from TextField styled

--- app/src/main/java/com/example/udos_wg_tohuwabohu/Calendar/CalendarAddFragment.kt
+++ app/src/main/java/com/example/udos_wg_tohuwabohu/Calendar/CalendarAddFragment.kt
@@ -1,18 +1,22 @@
 package com.example.udos_wg_tohuwabohu.Calendar
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Icon
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.EditText
+import android.widget.TimePicker
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -23,20 +27,27 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.compose.ui.window.Popup
+import androidx.fragment.app.Fragment
 import com.example.udos_wg_tohuwabohu.*
 import com.example.udos_wg_tohuwabohu.R
 import com.example.udos_wg_tohuwabohu.databinding.FragmentFinanceAddBinding
 import com.example.udos_wg_tohuwabohu.dataclasses.DBWriter
 import com.example.udos_wg_tohuwabohu.dataclasses.DataHandler
+import com.google.firebase.Timestamp
 import java.sql.Time
 import java.util.*
 
@@ -47,8 +58,9 @@ class CalendarAddFragment : Fragment() {
     private val dataHandler = DataHandler.getInstance()
     private val dbWriter = DBWriter.getInstance()
 
-    private val listCheckedNames = mutableStateMapOf<String, String>()
-
+    var dateChosen = mutableStateOf(Date())
+    var timeChosen = mutableStateOf(Time(Date().time))
+    var dbw = DBWriter.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -62,15 +74,22 @@ class CalendarAddFragment : Fragment() {
 
         val createFinanceButton: Button = _binding.buttonCreateFinance
         val cancelButton: Button = _binding.cancelCreateButton
-        val priceText: EditText = _binding.entryPrice
         val descriptionText: EditText = _binding.entryDescription
 
         createFinanceButton.setOnClickListener {
             val description = descriptionText.text.toString()
-            val price = priceText.text.toString().toDouble()
-            val moucherIDs = listCheckedNames.keys.toTypedArray()
+            val date = dateChosen.value
+            val time  = timeChosen.value
+            val summedDate = Date(date.year, date.month, date.date, time.hours, time.minutes)
+            val summedTimestamp = Timestamp(summedDate)
 
-            dbWriter.createFinanceEntry(description, price, moucherIDs.toList())
+            val validEntries = validateCalendarEntry(description, summedTimestamp)
+
+            if(validEntries)
+                dbw.createCalendarEntry(description, summedTimestamp)
+            else{
+                Log.w("Validation Error", "Please enter a description and a time in the future!")
+            }
             mainActivity.showFinanceFragment()
         }
 
@@ -85,122 +104,224 @@ class CalendarAddFragment : Fragment() {
         return view
     }
 
-    @OptIn(ExperimentalComposeUiApi::class)
     @Composable
-    fun DateAndTimePicker(){
-        var datePickerActive by rememberSaveable { mutableStateOf(false) }
-        var timePickerActive by rememberSaveable { mutableStateOf(false) }
+    fun DateAndTimePicker() {
+        val datePickerActive = rememberSaveable { mutableStateOf(false) }
+        val timePickerActive = rememberSaveable { mutableStateOf(false) }
         var dateChanged by rememberSaveable { mutableStateOf(false) }
         var timeChanged by rememberSaveable { mutableStateOf(false) }
-        var dateChosen by rememberSaveable { mutableStateOf(Date(0)) }
-        var timeChosen by rememberSaveable { mutableStateOf(Time(0, 0, 0)) }
 
-        if (timePickerActive) {
+        val focusRequester = remember { FocusRequester() }
+        val focusManager = LocalFocusManager.current
+
+        if (timePickerActive.value) {
             Box(
                 contentAlignment = Alignment.Center, // you apply alignment to all children
                 modifier = Modifier.fillMaxSize()
             ) {
                 Popup(
                     alignment = Alignment.Center,
-                    onDismissRequest = { timePickerActive = false },
+                    onDismissRequest = {
+                        focusManager.clearFocus(true)
+                        timePickerActive.value = false
+                    },
                     properties = UdoPopupProperties()
                 ) {
-                    showTimePicker(timeChosen, onTimeChange = {
-                        timeChosen = it
+                    showTimePicker(timeChosen.value, onTimeChange = {
+                        timeChosen.value = it
                         timeChanged = true
-                    }, onDismiss = { timePickerActive = it })
+                    }, onDismiss = { timePickerActive.value = it })
                 }
             }
-        } else if (datePickerActive) {
+        } else if (datePickerActive.value) {
             Box(
                 contentAlignment = Alignment.Center, // you apply alignment to all children
                 modifier = Modifier.fillMaxSize()
             ) {
                 Popup(
                     alignment = Alignment.Center,
-                    onDismissRequest = { datePickerActive = false },
+                    onDismissRequest = {
+                        focusManager.clearFocus(true)
+                        datePickerActive.value = false
+                    },
                     properties = UdoPopupProperties()
                 ) {
-                    showDatePicker(dateChosen, onDateChange = {
-                        dateChosen = it
+                    showDatePicker(dateChosen.value, onDateChange = {
+                        dateChosen.value = it
                         dateChanged = true
-                    }, onDismiss = { datePickerActive = it })
+                    }, onDismiss = { datePickerActive.value = it })
                 }
             }
         }
 
-        Row(modifier = Modifier.padding(5.dp)) {
+        Column {
+            Box(modifier = Modifier.padding(5.dp)) {
+                OutlinedTextFieldWithPopup(
+                    value = "${dateChosen.value.date}.${dateChosen.value.month + 1}.${dateChosen.value.year}",
+                    label = "Datum",
+                    popupBinding = datePickerActive,
+                    focusRequester = focusRequester,
+                    painter = painterResource(R.drawable.ic_baseline_calendar_month_24)
+                )
+            }
 
-            Button(
-                onClick = { datePickerActive = true },
+            Box(modifier = Modifier.padding(5.dp)) {
+                OutlinedTextFieldWithPopup(
+                    value = "${timeChosen.value.hours}:${timeChosen.value.minutes}",
+                    label = "Zeitpunk",
+                    popupBinding = timePickerActive,
+                    focusRequester = focusRequester,
+                    painter = painterResource(R.drawable.ic_baseline_access_time_24)
+                )
+            }
+        }
+
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun OutlinedTextFieldWithPopup(
+        value: String,
+        label: String,
+        popupBinding: MutableState<Boolean>,
+        focusRequester: FocusRequester,
+        painter: Painter
+    ) {
+
+        OutlinedTextField(
+            value = value, // "${dateChosen.date}.${dateChosen.month + 1}.${dateChosen.year}",
+            onValueChange = { },
+            readOnly = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(focusRequester)
+                .onFocusChanged { f ->
+                    popupBinding.value = (f.isFocused && f.hasFocus)
+                }
+                .padding(horizontal = 64.dp, vertical = 10.dp),
+            label = { Text(label) },
+            trailingIcon = {
+                Icon(
+                    painter,
+                    "$label auswählen",
+                    Modifier.clickable { popupBinding.value = !popupBinding.value },
+                    tint = UdoWhite
+                )
+            },
+            textStyle = TextStyle(
+                color = UdoWhite,
+                fontSize = 20.sp
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedLabelColor = UdoWhite,
+                focusedIndicatorColor = UdoWhite,
+                unfocusedLabelColor = UdoWhite,
+                unfocusedIndicatorColor = UdoWhite,
+                containerColor = Color.Transparent
+            )
+        )
+    }
+
+    @Composable
+    fun showDatePicker(
+        dateVar: Date,
+        onDateChange: (Date) -> Unit,
+        onDismiss: (Boolean) -> Unit
+    ) {
+        Card(
+            colors = UdoPopupCardTheme(),
+            modifier = Modifier
+                .requiredHeight(height = 450.dp)
+                .requiredWidth(width = 300.dp)
+                .padding(30.dp),
+            border = BorderStroke(2.dp, UdoDarkBlue)
+        ) {
+            AndroidView(
+                { CalendarView(it) },
                 modifier = Modifier
-                    .requiredWidth(200.dp)
+                    .wrapContentWidth()
+                    .background(UdoWhite, shape = RoundedCornerShape(4.dp)),
+                update = { views ->
+                    views.setOnDateChangeListener { calendarView, i, i2, i3 ->
+                        //var tempTimeVar = Timestamp(java.util.Date(i,i2+1,i3+1))
+                        onDateChange(Date(i, i2, i3))
+                    }
+                }
+            )
+            Button(
+                onClick = { onDismiss(false) },
+                modifier = Modifier
+                    .requiredWidth(190.dp)
                     .requiredHeight(50.dp)
                     .padding(5.dp)
+                    .align(Alignment.CenterHorizontally)
             ) {
                 Text(
-                    "  Datum Auswählen  ",
+                    "Speichern",
                     color = UdoWhite,
                     fontSize = 15.sp
                 )
             }
-            Card(
-                colors = UdoUnfocusableCardTheme(),
+        }
+    }
+
+    @Composable
+    fun showTimePicker(
+        timeVar: java.sql.Time,
+        onTimeChange: (java.sql.Time) -> Unit,
+        onDismiss: (Boolean) -> Unit
+    ) {
+        Card(
+            colors = UdoPopupCardTheme(),
+            modifier = Modifier
+                .requiredHeight(height = 500.dp)
+                .requiredWidth(width = 300.dp)
+                .padding(30.dp),
+            border = BorderStroke(2.dp, UdoDarkBlue)
+        ) {
+            AndroidView(
+                {
+                    var mTimePicker = TimePicker(it)
+                    mTimePicker.setIs24HourView(true)
+                    return@AndroidView mTimePicker
+                },
                 modifier = Modifier
-                    .requiredWidth(width = 100.dp)
-                    .requiredHeight(height = 50.dp)
-                    .padding(5.dp)
-            ) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    if (dateChanged) {
-                        Text(
-                            text = "${dateChosen.date}.${dateChosen.month + 1}.${dateChosen.year}",
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
+                    .wrapContentWidth()
+                    .background(UdoWhite, shape = RoundedCornerShape(4.dp)),
+                update = { views ->
+                    views.setOnTimeChangedListener { timePicker, hour, minute ->
+                        onTimeChange(Time(hour, minute, 0))
                     }
                 }
-            }
-        }
-
-        Row(modifier = Modifier.padding(5.dp)) {
+            )
             Button(
-                onClick = { timePickerActive = true },
+                onClick = { onDismiss(false) },
                 modifier = Modifier
-                    .requiredWidth(200.dp)
+                    .requiredWidth(190.dp)
                     .requiredHeight(50.dp)
-                    .padding(4.dp)
+                    .padding(5.dp)
+                    .align(Alignment.CenterHorizontally)
             ) {
                 Text(
-                    "Zeitpunkt Auswählen",
+                    "Speichern",
                     color = UdoWhite,
-                    fontSize = 15.sp,
-                    textAlign = TextAlign.Center
+                    fontSize = 15.sp
                 )
-            }
-            Card(
-                colors = UdoUnfocusableCardTheme(),
-                modifier = Modifier
-                    .requiredWidth(width = 100.dp)
-                    .requiredHeight(height = 50.dp)
-                    .padding(5.dp)
-            ) {
-                Box(modifier = Modifier.fillMaxSize()) {
-                    if (timeChanged) {
-                        Text(
-                            text = timeChosen.hours.toString() + ":"
-                                    + (timeChosen.minutes).toString()
-                                .padStart(2, "0".single()),
-                            textAlign = TextAlign.Center,
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                }
             }
         }
     }
 
+    fun validateCalendarEntry(message: String, summedTimestamp: Timestamp): Boolean {
+        return  (summedTimestamp.seconds > Timestamp.now().seconds) and (message != "")
+    }
+
     fun setMainActivity(mainActivity: MainActivity) {
         this.mainActivity = mainActivity
+    }
+
+    @Preview
+    @Composable
+    fun test() {
+        DateAndTimePicker()
     }
 }
